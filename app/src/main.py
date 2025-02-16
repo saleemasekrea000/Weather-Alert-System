7c92bdcd42f3a98e8dedae217e800b17273ca334
saleemasekrea000 <s.asekrea@innopolis.university>
2025-02-16 13:10:10 +0300

    use lifespan instead of onevent

--- app/src/main.py
+++ app/src/main.py
@@ -1,24 +1,23 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 
 from src.redis_client import r as redis_client
 from src.routers import alert, weather
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Startup logic
+    await redis_client.init()
+
+    # Yield control back to FastAPI
+    yield
+
+    await redis_client.close()
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(weather.weather_router)
 app.include_router(alert.alert_router)
-
-
-@app.on_event("startup")
-async def startup():
-    # Initialize Redis connection when app starts.
-    await redis_client.init()
-
-
-@app.on_event("shutdown")
-async def shutdown():
-    # Close Redis connection when app stops.
-    await redis_client.close()
 
 
 @app.get("/")
